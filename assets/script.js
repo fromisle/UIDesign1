08463b0b1fd5b4de0a274eb9ed3bae69f9ac258a
fromisle <156303012+fromisle@users.noreply.github.com>
2024-06-24 17:46:34 +0900

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
     const sections = document.querySelectorAll('.section');
     let currentSection = 0;
+    let isScrolling = false; // 스크롤 상태를 추적하기 위한 변수
 
     function scrollToSection(index) {
-        if (index >= 0 && index < sections.length) {
+        if (index >= 0 && index < sections.length && !isScrolling) {
+            isScrolling = true;
             sections[index].scrollIntoView({ behavior: 'smooth' });
             currentSection = index;
+
+            // 스크롤이 완료된 후 isScrolling을 false로 설정
+            setTimeout(() => {
+                isScrolling = false;
+            }, 1000); // 스크롤 애니메이션 시간에 맞춰 조정
         }
     }
 
     window.addEventListener('wheel', (event) => {
-        if (event.deltaY > 0) {
-            scrollToSection(currentSection + 1);
-        } else {
-            scrollToSection(currentSection - 1);
+        if (!isScrolling) {
+            if (event.deltaY > 0) {
+                scrollToSection(currentSection + 1);
+            } else {
+                scrollToSection(currentSection - 1);
+            }
         }
     });
 
     window.addEventListener('keydown', (event) => {
-        if (event.key === 'ArrowDown') {
-            scrollToSection(currentSection + 1);
-        } else if (event.key === 'ArrowUp') {
-            scrollToSection(currentSection - 1);
+        if (!isScrolling) {
+            if (event.key === 'ArrowDown') {
+                scrollToSection(currentSection + 1);
+            } else if (event.key === 'ArrowUp') {
+                scrollToSection(currentSection - 1);
+            }
         }
     });
 });
 
-// 스크롤 다운 //
+// 스크롤 다운 버튼
 document.addEventListener("DOMContentLoaded", function() {
     const scrollButton = document.getElementById('scroll-button');
     
